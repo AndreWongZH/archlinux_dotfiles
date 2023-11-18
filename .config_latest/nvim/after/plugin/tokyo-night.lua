47f93788a51b326be2da470921d83ffc949a7b78
AndreWongZH <wongandre98@gmail.com>
2023-11-18 13:38:55 +0800

    add

---
+++ .config_latest/nvim/after/plugin/tokyo-night.lua
@@ -0,0 +1,3 @@
+-- set neovim color theme
+vim.cmd[[colorscheme tokyonight-storm]]
+vim.cmd('highlight LineNr guifg=#0ec48b')
---
+++ .config_latest/nvim/lua/basic/set.lua
@@ -0,0 +1,23 @@
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
